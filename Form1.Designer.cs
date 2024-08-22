7422146f5b813bc8bfe0680ba68643e2d6cd2e8b
Fikret ŞAFAK <fsafak24@gmail.com>
2024-08-22 16:39:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace DevicePingController
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.devNameLbl = new System.Windows.Forms.TextBox();
+            this.DevIPlbl = new System.Windows.Forms.TextBox();
+            this.DeviceAddButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.devList = new System.Windows.Forms.ListView();
+            this.DevNameLst = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DevIPLst = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DeviceImportBtn = new System.Windows.Forms.Button();
+            this.DeviceExportBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pingStatDGW = new System.Windows.Forms.DataGridView();
+            this.DeviceDGW = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IpAddressDGW = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StatusDGW = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pingBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pingStatDGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // devNameLbl
+            // 
+            this.devNameLbl.Location = new System.Drawing.Point(13, 244);
+            this.devNameLbl.Name = "devNameLbl";
+            this.devNameLbl.Size = new System.Drawing.Size(127, 20);
+            this.devNameLbl.TabIndex = 1;
+            // 
+            // DevIPlbl
+            // 
+            this.DevIPlbl.Location = new System.Drawing.Point(146, 244);
+            this.DevIPlbl.Name = "DevIPlbl";
+            this.DevIPlbl.Size = new System.Drawing.Size(125, 20);
+            this.DevIPlbl.TabIndex = 2;
+            // 
+            // DeviceAddButton
+            // 
+            this.DeviceAddButton.Location = new System.Drawing.Point(12, 270);
+            this.DeviceAddButton.Name = "DeviceAddButton";
+            this.DeviceAddButton.Size = new System.Drawing.Size(259, 23);
+            this.DeviceAddButton.TabIndex = 3;
+            this.DeviceAddButton.Text = "Device Add";
+            this.DeviceAddButton.UseVisualStyleBackColor = true;
+            this.DeviceAddButton.Click += new System.EventHandler(this.DeviceAddButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Device Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP Address";
+            // 
+            // devList
+            // 
+            this.devList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.DevNameLst,
+            this.DevIPLst});
+            this.devList.HideSelection = false;
+            this.devList.Location = new System.Drawing.Point(15, 12);
+            this.devList.Name = "devList";
+            this.devList.Size = new System.Drawing.Size(246, 211);
+            this.devList.TabIndex = 6;
+            this.devList.UseCompatibleStateImageBehavior = false;
+            this.devList.View = System.Windows.Forms.View.Details;
+            this.devList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.devList_MouseDoubleClick_1);
+            // 
+            // DevNameLst
+            // 
+            this.DevNameLst.Text = "Device Name";
+            this.DevNameLst.Width = 100;
+            // 
+            // DevIPLst
+            // 
+            this.DevIPLst.Text = "Device IP";
+            this.DevIPLst.Width = 140;
+            // 
+            // DeviceImportBtn
+            // 
+            this.DeviceImportBtn.Location = new System.Drawing.Point(13, 299);
+            this.DeviceImportBtn.Name = "DeviceImportBtn";
+            this.DeviceImportBtn.Size = new System.Drawing.Size(259, 23);
+            this.DeviceImportBtn.TabIndex = 7;
+            this.DeviceImportBtn.Text = "Device Import";
+            this.DeviceImportBtn.UseVisualStyleBackColor = true;
+            this.DeviceImportBtn.Click += new System.EventHandler(this.DeviceImportBtn_Click);
+            // 
+            // DeviceExportBtn
+            // 
+            this.DeviceExportBtn.Location = new System.Drawing.Point(13, 328);
+            this.DeviceExportBtn.Name = "DeviceExportBtn";
+            this.DeviceExportBtn.Size = new System.Drawing.Size(259, 23);
+            this.DeviceExportBtn.TabIndex = 8;
+            this.DeviceExportBtn.Text = "Device Export";
+            this.DeviceExportBtn.UseVisualStyleBackColor = true;
+            this.DeviceExportBtn.Click += new System.EventHandler(this.DeviceExportBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // pingStatDGW
+            // 
+            this.pingStatDGW.AllowUserToAddRows = false;
+            this.pingStatDGW.AllowUserToDeleteRows = false;
+            this.pingStatDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pingStatDGW.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DeviceDGW,
+            this.IpAddressDGW,
+            this.StatusDGW});
+            this.pingStatDGW.Location = new System.Drawing.Point(267, 12);
+            this.pingStatDGW.Name = "pingStatDGW";
+            this.pingStatDGW.Size = new System.Drawing.Size(521, 211);
+            this.pingStatDGW.TabIndex = 9;
+            // 
+            // DeviceDGW
+            // 
+            this.DeviceDGW.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DeviceDGW.HeaderText = "Device Name";
+            this.DeviceDGW.Name = "DeviceDGW";
+            this.DeviceDGW.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // IpAddressDGW
+            // 
+            this.IpAddressDGW.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.IpAddressDGW.HeaderText = "Device IP";
+            this.IpAddressDGW.Name = "IpAddressDGW";
+            // 
+            // StatusDGW
+            // 
+            this.StatusDGW.HeaderText = "Ping Status";
+            this.StatusDGW.Name = "StatusDGW";
+            this.StatusDGW.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // pingBtn
+            // 
+            this.pingBtn.Location = new System.Drawing.Point(288, 244);
+            this.pingBtn.Name = "pingBtn";
+            this.pingBtn.Size = new System.Drawing.Size(259, 23);
+            this.pingBtn.TabIndex = 10;
+            this.pingBtn.Text = "Device Add";
+            this.pingBtn.UseVisualStyleBackColor = true;
+            this.pingBtn.Click += new System.EventHandler(this.pingBtn_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 362);
+            this.Controls.Add(this.pingBtn);
+            this.Controls.Add(this.pingStatDGW);
+            this.Controls.Add(this.DeviceExportBtn);
+            this.Controls.Add(this.DeviceImportBtn);
+            this.Controls.Add(this.devList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeviceAddButton);
+            this.Controls.Add(this.DevIPlbl);
+            this.Controls.Add(this.devNameLbl);
+            this.Name = "Form_Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pingStatDGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox devNameLbl;
+        private System.Windows.Forms.TextBox DevIPlbl;
+        private System.Windows.Forms.Button DeviceAddButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView devList;
+        private System.Windows.Forms.ColumnHeader DevNameLst;
+        private System.Windows.Forms.ColumnHeader DevIPLst;
+        private System.Windows.Forms.Button DeviceImportBtn;
+        private System.Windows.Forms.Button DeviceExportBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView pingStatDGW;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DeviceDGW;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IpAddressDGW;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StatusDGW;
+        private System.Windows.Forms.Button pingBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Timers;
+
+namespace DevicePingController
+{
+    public partial class Form_Main : Form
+    {
+        string devInfo;
+        string[] tempDevice;
+        string tempIP;
+        DataTable pingTable = new DataTable();
+        List<string> ipAddress = new List<string>();
+        List<PictureBox> pictureboxList = new List<PictureBox>();
+        private System.Timers.Timer timer;
+        public Form_Main()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        private void FillPingTable()
+        {
+            pingTable.Columns.Add("ip",typeof(string));
+            pingTable.Columns.Add("picturebox ", typeof(string));
+
+            pingTable.Rows.Add();
+
+            for (int i = 0; i < devList.Items.Count;i++)
+            {
+                pingTable.Rows.Add(devList.Items[i].ToString(), pictureboxList[i]);
+            }
+        }
+        private void DeviceAddButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(devNameLbl.Text) || string.IsNullOrEmpty(DevIPlbl.Text))
+                return;
+            ListViewItem item = new ListViewItem(devNameLbl.Text);
+            item.SubItems.Add(DevIPlbl.Text);
+            devList.Items.Add(item);
+            devNameLbl.Clear();
+            DevIPlbl.Clear();
+            dgwFill();
+
+        }
+
+        private void devList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            timer=new System.Timers.Timer();
+            timer.Interval = 10000;
+            timer.Enabled = true;
+            timer.Elapsed += timer_Elapsed;
+
+        }
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void devList_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            if (devList.Items.Count > 0)
+            {
+                devList.Items.Remove(devList.SelectedItems[0]);
+            }
+        }
+
+        private void DeviceExportBtn_Click(object sender, EventArgs e)
+        {
+                SaveFileDialog save = new SaveFileDialog();
+                save.CreatePrompt = true; // dosya yoksa üret
+                save.OverwritePrompt = true; // üzerine yazma uyarısı
+                save.Title = "CSV Dosyası";
+                save.DefaultExt = "csv";
+                save.Filter = "Veri Dosyaları (*.csv)|*.csv|Tüm Dosyalar(*.*)|*.*";
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    if (false)
+                    {
+                        // Burada AppandText metodunu kullanmak için FileStream kullandık. Bunun amacı farklı yöntemleri görmenizi istememdir.
+                        FileStream fs = new FileStream(save.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                        fs.Close();
+                        File.AppendAllText(save.FileName, Environment.NewLine);
+                    }
+                    else
+                    {
+                        if (File.Exists(save.FileName))
+                        {
+                            StreamReader Oku = new StreamReader(save.FileName);
+                            string okunan = Oku.ReadToEnd();
+                            Oku.Close();
+                            if (okunan.Trim() != string.Empty)
+                            {
+                                switch (MessageBox.Show("Seçtiğiniz belge boş değil. Üzerine yazmak istiyorsanız -EVET-, ekrana getirmek istiyorsanız -HAYIR-, işlemi iptal etmek istiyorsanız -VAZGEÇ-'i seçin"
+                                , "İşlem Seçin", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+                                {
+                                    case DialogResult.Cancel:
+                                        break;
+                                    case DialogResult.Yes:
+                                        StreamWriter sw = new StreamWriter(save.FileName, true);
+                                        for (int i = 0; i < devList.Items.Count; i++)
+                                        {
+                                            sw.WriteLine(devList.Items[i].Text.ToString() + ";" + devList.Items[i].SubItems[1].Text.ToString());
+                                        }
+                                        sw.Close();
+                                        MessageBox.Show("Device exported.");
+                                        break;
+                                    case DialogResult.No:
+
+                                        break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            StreamWriter sw = new StreamWriter(save.FileName, true);
+                            for (int i = 0; i < devList.Items.Count; i++)
+                            {
+                                sw.WriteLine(devList.Items[i].Text.ToString() + ";" + devList.Items[i].SubItems[1].Text.ToString());
+                            }
+                            sw.Close();
+                             MessageBox.Show("Device exported.");
+                    }
+                    }
+                }
+
+
+            
+        }
+
+        private void DeviceImportBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "txt Dosyaları (*.txt)|*.txt|Tüm Dosyalar(*.*)|*.*";
+            file.FilterIndex = 2;
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.Title = "Metin Dosyası Seçiniz..";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(file.FileName);
+                devList.Items.Clear();
+                while (sr.EndOfStream==false)
+                {
+                    string line = sr.ReadLine();
+                    tempDevice = line.Split(';');
+                    ListViewItem item = new ListViewItem(tempDevice[0]);
+                    item.SubItems.Add(tempDevice[1]);
+                    devList.Items.Add(item);
+                }
+                sr.Close();
+                dgwFill();
+                MessageBox.Show("Device imported.");
+            }
+        }
+
+        private void dgwFill()
+        {
+            pingStatDGW.Rows.Clear();
+            for (int i = 0; i < devList.Items.Count; i++)
+            {
+                pingStatDGW.Rows.Add();
+                pingStatDGW.Rows[i].Cells[0].Value = devList.Items[i].SubItems[0].Text;
+                pingStatDGW.Rows[i].Cells[1].Value = devList.Items[i].SubItems[1].Text;
+                // pingStatDGW.Rows[i].Cells[2].Value = false;
+            }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pingBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(500);
+                Parallel.For(0, devList.Items.Count, (i, loopState) =>
+                {
+                    Ping ping = new Ping();
+                    PingReply pingReply = ping.Send(pingStatDGW.Rows[i].Cells[1].ToString());
+                    this.BeginInvoke((Action)delegate ()
+                    {
+                        if (pingReply.Status == IPStatus.Success)
+                        {
+                            pingStatDGW.Rows[i].Cells[1].Value = "Succesful";
+                        }
+                        else
+                        {
+                            pingStatDGW.Rows[i].Cells[2].Value = "Not Pinged";
+                        }
+
+                    });
+                });
+
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
